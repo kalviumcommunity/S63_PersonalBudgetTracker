64f3261b204b0bb6910f6e0f495faeb97051da10
anujgoyaleng <anuj.goyal@kalvium.community>
2025-01-30 10:54:45 +0530

    adding bruno

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.get('/ping', (req, res) => {
     });
 
     
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(8000, () => {
+    console.log('Server is running on port 8000');
      });
 
