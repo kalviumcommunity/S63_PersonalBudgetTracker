793502ccd34a0ba22ae8f3dfeee38baf8761ec03
anujgoyaleng <anuj.goyal@kalvium.community>
2025-02-05 13:32:56 +0530

    connect databse to mongodb

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+
+
+const express = require("express");
+const connectDB = require("./db");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB
+connectDB();
+
+app.get("/", (req, res) => {
+    res.send("MongoDB Atlas is connected to VS Code!");
+});
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
